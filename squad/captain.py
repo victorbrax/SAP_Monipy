0df9c2caac1cee9f495f1f6798a203f13e413dff
victorbrax <joaogomespcl@gmail.com>
2022-12-28 01:51:00 -0300

    Soldier Mess

---
+++ squad/captain.py
@@ -0,0 +1,18 @@
+from lodge import *
+
+def check_cons(env, templ):
+    for sapenv in env:
+        for count, saptempl in enumerate(templ):
+            if sapenv == 'ed1':
+                cED1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+            if sapenv == 'eq1':
+                cEQ1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+            if sapenv == 'ep1':
+                cEP1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+            if sapenv == 'grb':
+                cGRB.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+            if sapenv == 'grc':
+                cGRC.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+            if sapenv == 'slm':
+                cSLM.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+    return
