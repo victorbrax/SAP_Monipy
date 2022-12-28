ca70bad7962d371a3f35f4655171b4d3df43e5dc
victorbrax <joaogomespcl@gmail.com>
2022-12-28 02:02:40 -0300

    Sleep hour

--- squad/captain.py
+++ squad/captain.py
@@ -1,18 +1,18 @@
-from lodge import *
+from squad.lodge import *
 
-def check_cons(env, templ):
+def gen_commands(env, templ):
     for sapenv in env:
         for count, saptempl in enumerate(templ):
             if sapenv == 'ed1':
-                cED1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cED1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
             if sapenv == 'eq1':
-                cEQ1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cEQ1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
             if sapenv == 'ep1':
-                cEP1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cEP1.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
             if sapenv == 'grb':
-                cGRB.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cGRB.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
             if sapenv == 'grc':
-                cGRC.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cGRC.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
             if sapenv == 'slm':
-                cSLM.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > {sapenv}_FILE{count+1}.txt')
+                cSLM.append(f'./../src/check_sap_cons {sapenv}_{saptempl} SLM > test/{sapenv}_FILE{count+1}.txt')
     return
